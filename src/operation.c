10dc47417f804923f21eea17395e4d44d697e065
Matthieu Dorier <mdorier@anl.gov>
2016-02-08 07:04:57 -0600

    supporting alltoallv

--- src/operation.c
+++ src/operation.c
@@ -44,6 +44,161 @@ static void* copy_parameters(dumpi_function type, const void *prm) {
 	unsigned int size = typesize[type];
 	void* result = malloc(size);
 	memcpy(result,prm,size);
+	// here we handle the case of copying pointers
+	// this should be done case-by case
+	// TODO: for now all "status"-type pointers are ignored
+	// TODO: not all the types are handles bellow
+#define COPY(_fun,_type,_field,_size_field) {	\
+	((_fun*)result)->_field = (_type*)malloc((((_fun*)prm)->_size_field)*sizeof(_type)); \
+	memcpy(((_fun*)result)->_field,((_fun*)prm)->_field,(((_fun*)prm)->_size_field)*sizeof(_type)); }
+	
+	switch(type) {
+	case DUMPI_Waitany:
+		COPY(dumpi_waitany,dumpi_request,requests,count)
+		break;
+	case DUMPI_Testany:
+		COPY(dumpi_testany,dumpi_request,requests,count)
+		break;
+	case DUMPI_Waitall:
+		COPY(dumpi_waitall,dumpi_request,requests,count)
+		break;
+	case DUMPI_Testall:
+		COPY(dumpi_testall,dumpi_request,requests,count)
+		break;
+	case DUMPI_Waitsome:
+		COPY(dumpi_waitsome,dumpi_request,requests,count)
+		break;
+	case DUMPI_Testsome:
+		COPY(dumpi_testsome,dumpi_request,requests,count)
+		break;
+	case DUMPI_Startall:
+		COPY(dumpi_startall,dumpi_request,requests,count)
+		break;
+	case DUMPI_Type_indexed:
+		COPY(dumpi_type_indexed,int,lengths,count)
+		COPY(dumpi_type_indexed,int,indices,count)
+		break;
+	case DUMPI_Type_hindexed:
+		// TODO
+	case DUMPI_Type_struct:
+
+	case DUMPI_Gatherv:
+
+	case DUMPI_Scatterv:
+
+	case DUMPI_Allgatherv:
+
+	case DUMPI_Alltoallv:
+
+	case DUMPI_Reduce_scatter:
+
+	case DUMPI_Group_translate_ranks:
+
+	case DUMPI_Group_incl:
+
+	case DUMPI_Group_excl:
+
+	case DUMPI_Group_range_incl:
+
+	case DUMPI_Group_range_excl:
+
+	case DUMPI_Cart_create:
+
+	case DUMPI_Dims_create:
+
+	case DUMPI_Graph_create:
+
+	case DUMPI_Graph_get:
+
+	case DUMPI_Cart_get:
+
+	case DUMPI_Cart_rank:
+
+	case DUMPI_Cart_coords:
+
+	case DUMPI_Graph_neighbors:
+
+	case DUMPI_Cart_sub:
+
+	case DUMPI_Cart_map:
+
+	case DUMPI_Graph_map:
+
+	case DUMPI_Get_processor_name:
+
+	case DUMPI_Error_string:
+
+	case DUMPI_Init:
+
+	case DUMPI_Close_port:
+
+	case DUMPI_Comm_accept:
+
+	case DUMPI_Comm_connect:
+
+	case DUMPI_Comm_spawn:
+
+	case DUMPI_Comm_spawn_multiple:
+
+	case DUMPI_Lookup_name:
+
+	case DUMPI_Open_port:
+
+	case DUMPI_Publish_name:
+
+	case DUMPI_Unpublish_name:
+
+	case DUMPI_Alltoallw:
+
+	case DUMPI_Add_error_string:
+
+	case DUMPI_Comm_get_name:
+
+	case DUMPI_Comm_set_name:
+
+	case DUMPI_Init_thread:
+
+	case DUMPI_Type_get_contents:
+
+	case DUMPI_Type_get_name:
+
+	case DUMPI_Type_set_name:
+
+	case DUMPI_Info_get:
+
+	case DUMPI_Info_get_nthkey:
+
+	case DUMPI_Info_get_valuelen:
+
+	case DUMPI_Info_set:
+
+	case DUMPI_Pack_external:
+
+	case DUMPI_Pack_external_size:
+
+	case DUMPI_Type_create_darray:
+
+	case DUMPI_Type_create_hindexed:
+
+	case DUMPI_Type_create_indexed_block:
+
+	case DUMPI_Type_create_struct:
+
+	case DUMPI_Type_create_subarray:
+
+	case DUMPI_Unpack_external:
+
+	case DUMPI_File_open:
+
+	case DUMPI_File_delete:
+
+	case DUMPI_File_set_view:
+
+	case DUMPI_File_get_view:
+
+		fprintf(stderr,"Error in file %s line %d, please see developer",__FILE__,__LINE__);
+		exit(-1);
+	}
 	return result;
 }
 
---
+++ src/translations/mpi_alltoallv.c
@@ -0,0 +1,86 @@
+#include "cortex/cortex.h"
+
+/**
+ * This translates MPI_Alltoallv calls into a series of
+ * point to point calls. The following
+ * algorithm was found in the Mpich implementation.
+ */
+int cortex_translate_MPI_Alltoallv(const dumpi_alltoallv *prm, 
+			uint16_t thread, 
+			const dumpi_time *cpu, 
+			const dumpi_time *wall,
+			const dumpi_perfinfo *perf,
+			void *uarg) {
+
+	int comm_size, i, j;
+	int dst, rank, req_cnt, req_num = 1;
+	int ii, ss, bblock;
+	int type_size;
+
+	dumpi_request* reqarray;
+	dumpi_status* starray;
+
+	dumpi_irecv irecv_param;
+	dumpi_isend isend_param;
+	dumpi_waitall waitall_param;
+
+	cortex_comm_get_size(uarg, prm->comm, &comm_size);
+	rank = thread;
+
+	bblock = 32; // this is actuall controlled by MPIR_CVAR_ALLTOALL_THROTTLE
+
+	starray = (dumpi_status*)malloc(2*bblock*sizeof(dumpi_status));
+	reqarray = (dumpi_request*)malloc(2*bblock*sizeof(dumpi_request));
+	
+	for(ii=0; ii<comm_size; ii+=bblock) {
+
+		req_cnt = 0;
+		ss = comm_size-ii < bblock ? comm_size-ii : bblock;
+
+		for ( i=0; i<ss; i++ ) {
+			dst = (rank+i+ii) % comm_size;
+			if (prm->recvcounts[dst]) {
+				
+					irecv_param.count	= prm->recvcounts[dst];
+					irecv_param.datatype	= prm->recvtype;
+					irecv_param.source	= dst;
+					irecv_param.tag		= 1234;
+					irecv_param.comm	= prm->comm;
+					irecv_param.request	= req_num++; // hopefuly the program is not doing other requests at the same time...
+					reqarray[req_cnt] = irecv_param.request;
+					cortex_post_MPI_Irecv(&irecv_param, thread, cpu, wall, perf, uarg);
+
+					req_cnt++;
+			}
+		}
+
+		for ( i=0; i<ss; i++ ) {
+			dst = (rank-i-ii+comm_size) % comm_size;
+			if (prm->sendcounts[dst]) {
+
+					isend_param.count	= prm->sendcounts[dst];
+					isend_param.datatype	= prm->sendtype;
+					isend_param.dest 	= dst;
+					isend_param.tag		= 1234;
+					isend_param.comm	= prm->comm;
+					isend_param.request	= req_num++;
+
+					reqarray[req_cnt] = isend_param.request;
+					cortex_post_MPI_Isend(&isend_param, thread, cpu, wall, perf, uarg);
+
+					req_cnt++;
+			}
+		}
+
+		waitall_param.count = req_cnt;
+		waitall_param.requests = reqarray;
+		waitall_param.statuses = starray;
+
+		cortex_post_MPI_Waitall(&waitall_param, thread, cpu, wall, perf, uarg);
+	}
+
+	free(starray);
+	free(reqarray);
+
+	return 0;
+}
