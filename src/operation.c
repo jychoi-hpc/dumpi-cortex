0df1bc33b04e334c5292e902d3a19963b4ad818c
Matthieu Dorier <mdorier@anl.gov>
2016-02-09 13:53:10 -0600

    implemented all the deap copy operations

--- src/operation.c
+++ src/operation.c
@@ -46,12 +46,14 @@ static void* copy_parameters(dumpi_function type, const void *prm) {
 	memcpy(result,prm,size);
 	// here we handle the case of copying pointers
 	// this should be done case-by case
-	// TODO: for now all "status"-type pointers are ignored
-	// TODO: not all the types are handles bellow
-#define COPY(_fun,_type,_field,_size_field) {	\
-	((_fun*)result)->_field = (_type*)malloc((((_fun*)prm)->_size_field)*sizeof(_type)); \
-	memcpy(((_fun*)result)->_field,((_fun*)prm)->_field,(((_fun*)prm)->_size_field)*sizeof(_type)); }
-	
+#define COPY2(_fun,_type,_field,_size_field,_mult) {	\
+	((_fun*)result)->_field = (_type*)malloc((((_fun*)prm)->_size_field)*sizeof(_type)*_mult); \
+	memcpy(((_fun*)result)->_field,((_fun*)prm)->_field,(((_fun*)prm)->_size_field)*sizeof(_type)*_mult); }
+
+#define COPY(_fun,_type,_field,_size_field) COPY2(_fun,_type,_field,_size_field,1)
+
+#define STRCOPY(_fun,_field) ((_fun*)result)->_field = strdup(((_fun*)prm)->_field);
+	int i,j;
 	switch(type) {
 	case DUMPI_Waitany:
 		COPY(dumpi_waitany,dumpi_request,requests,count)
@@ -79,125 +81,249 @@ static void* copy_parameters(dumpi_function type, const void *prm) {
 		COPY(dumpi_type_indexed,int,indices,count)
 		break;
 	case DUMPI_Type_hindexed:
-		// TODO
+		COPY(dumpi_type_hindexed,int,lengths,count)
+		COPY(dumpi_type_hindexed,int,indices,count)
+		break;
 	case DUMPI_Type_struct:
-
+		COPY(dumpi_type_struct,int,lengths,count)
+		COPY(dumpi_type_struct,int,indices,count)
+		COPY(dumpi_type_struct,dumpi_datatype,oldtypes,count)
+		break;
 	case DUMPI_Gatherv:
-
+		COPY(dumpi_gatherv,int,recvcounts,commsize)
+		COPY(dumpi_gatherv,int,displs,commsize)
+		break;
 	case DUMPI_Scatterv:
-
+		COPY(dumpi_scatterv,int,sendcounts,commsize)
+		COPY(dumpi_scatterv,int,displs,commsize)
+		break;
 	case DUMPI_Allgatherv:
-
+		COPY(dumpi_allgatherv,int,recvcounts,commsize)
+		COPY(dumpi_allgatherv,int,displs,commsize)
+		break;
 	case DUMPI_Alltoallv:
-
+		COPY(dumpi_alltoallv,int,sendcounts,commsize)
+		COPY(dumpi_alltoallv,int,senddispls,commsize)
+		COPY(dumpi_alltoallv,int,recvcounts,commsize)
+		COPY(dumpi_alltoallv,int,recvdispls,commsize)
+		break;
 	case DUMPI_Reduce_scatter:
-
+		COPY(dumpi_reduce_scatter,int,recvcounts,commsize)
+		break;
 	case DUMPI_Group_translate_ranks:
-
+		COPY(dumpi_group_translate_ranks,int,ranks1,count)
+		COPY(dumpi_group_translate_ranks,int,ranks2,count)
+		break;
 	case DUMPI_Group_incl:
-
+		COPY(dumpi_group_incl,int,ranks,count)
+		break;
 	case DUMPI_Group_excl:
-
+		COPY(dumpi_group_excl,int,ranks,count)
+		break;
 	case DUMPI_Group_range_incl:
-
+		COPY2(dumpi_group_range_incl,int,ranges,count,3)
+		break;
 	case DUMPI_Group_range_excl:
-
+		COPY2(dumpi_group_range_excl,int,ranges,count,3)
+		break;
 	case DUMPI_Cart_create:
-
+		COPY(dumpi_cart_create,int,dims,ndim)
+		COPY(dumpi_cart_create,int,periods,ndim)
+		break;
 	case DUMPI_Dims_create:
-
+		COPY(dumpi_dims_create,int,dims.in,ndim)
+		COPY(dumpi_dims_create,int,dims.out,ndim)
+		break;
 	case DUMPI_Graph_create:
-
+		COPY(dumpi_graph_create,int,index,nodes)
+		COPY(dumpi_graph_create,int,edges,numedges)
+		break;
 	case DUMPI_Graph_get:
-
+		COPY(dumpi_graph_get,int,index,totnodes)
+		COPY(dumpi_graph_get,int,edges,totedges)
+		break;
 	case DUMPI_Cart_get:
-
+		COPY(dumpi_cart_get,int,dims,ndim)
+		COPY(dumpi_cart_get,int,periods,ndim)
+		COPY(dumpi_cart_get,int,coords,ndim)
+		break;
 	case DUMPI_Cart_rank:
-
+		COPY(dumpi_cart_rank,int,coords,ndim)
+		break;
 	case DUMPI_Cart_coords:
-
+		COPY(dumpi_cart_coords,int,coords,ndim)
+		break;
 	case DUMPI_Graph_neighbors:
-
+		COPY(dumpi_graph_neighbors,int,neighbors,nneigh)
+		break;
 	case DUMPI_Cart_sub:
-
+		COPY(dumpi_cart_sub,int,remain_dims,ndim)
+		break;
 	case DUMPI_Cart_map:
-
+		COPY(dumpi_cart_map,int,dims,ndim)
+		COPY(dumpi_cart_map,int,period,ndim)
+		break;
 	case DUMPI_Graph_map:
-
+		COPY(dumpi_graph_map,int,index,nodes)
+		COPY(dumpi_graph_map,int,edges,numedges)
+		break;
 	case DUMPI_Get_processor_name:
-
+		STRCOPY(dumpi_get_processor_name,name)
+		break;
 	case DUMPI_Error_string:
-
+		STRCOPY(dumpi_error_string,errorstring)
+		break;
 	case DUMPI_Init:
-
+		((dumpi_init*)result)->argv = (char**)malloc(sizeof(char*)*((dumpi_init*)result)->argc);
+		for(i=0; i<((dumpi_init*)prm)->argc; i++) {
+			STRCOPY(dumpi_init,argv[i])
+		}
+		break;
 	case DUMPI_Close_port:
-
+		STRCOPY(dumpi_close_port,portname)
+		break;
 	case DUMPI_Comm_accept:
-
+		STRCOPY(dumpi_comm_accept,portname)
+		break;
 	case DUMPI_Comm_connect:
-
+		STRCOPY(dumpi_comm_connect,portname)
+		break;
 	case DUMPI_Comm_spawn:
-
+		if(((dumpi_comm_spawn*)prm)->oldcommrank == ((dumpi_comm_spawn*)prm)->root) {
+			STRCOPY(dumpi_comm_spawn,command)
+			i = 0;
+			while(((dumpi_comm_spawn*)prm)->argv[i] != NULL) i++;
+			((dumpi_comm_spawn*)result)->argv = (char**)malloc(sizeof(char*)*(i+1));
+			i = 0;
+			while(((dumpi_comm_spawn*)prm)->argv[i] != NULL) {
+				STRCOPY(dumpi_comm_spawn,argv[i])
+				i++;
+			}
+		}
+		break;
 	case DUMPI_Comm_spawn_multiple:
-
+		if(((dumpi_comm_spawn_multiple*)prm)->oldcommrank == ((dumpi_comm_spawn_multiple*)prm)->root) {
+			((dumpi_comm_spawn_multiple*)result)->commands = (char**)malloc(sizeof(char*)*((dumpi_comm_spawn_multiple*)prm)->count);
+			((dumpi_comm_spawn_multiple*)result)->argvs = (char***)malloc(sizeof(char**)*((dumpi_comm_spawn_multiple*)prm)->count);
+			for(j=0; j < ((dumpi_comm_spawn_multiple*)prm)->count; j++) {
+				STRCOPY(dumpi_comm_spawn_multiple,commands[j])
+				i = 0;
+				while(((dumpi_comm_spawn_multiple*)prm)->argvs[j][i] != NULL) i++;
+				((dumpi_comm_spawn_multiple*)result)->argvs[j] = (char**)malloc(sizeof(char*)*(i+1));
+				i = 0;
+				 while(((dumpi_comm_spawn_multiple*)prm)->argvs[j][i] != NULL) {
+					STRCOPY(dumpi_comm_spawn_multiple,argvs[j][i])
+					i++;
+				}
+			}
+		}
+		break;
 	case DUMPI_Lookup_name:
-
+		STRCOPY(dumpi_lookup_name,servicename)
+		STRCOPY(dumpi_lookup_name,portname)
+		break;
 	case DUMPI_Open_port:
-
+		STRCOPY(dumpi_open_port,portname)
+		break;
 	case DUMPI_Publish_name:
-
+		STRCOPY(dumpi_publish_name,servicename)
+		STRCOPY(dumpi_publish_name,portname)
+		break;
 	case DUMPI_Unpublish_name:
-
+		STRCOPY(dumpi_unpublish_name,servicename)
+		STRCOPY(dumpi_unpublish_name,portname)
+		break;
 	case DUMPI_Alltoallw:
-
+		COPY(dumpi_alltoallw,int,sendcounts,commsize)
+		COPY(dumpi_alltoallw,int,senddispls,commsize)
+		COPY(dumpi_alltoallw,dumpi_datatype,sendtypes,commsize)
+		COPY(dumpi_alltoallw,int,recvcounts,commsize)
+		COPY(dumpi_alltoallw,int,recvdispls,commsize)
+		COPY(dumpi_alltoallw,dumpi_datatype,recvtypes,commsize)
+		break;
 	case DUMPI_Add_error_string:
-
+		STRCOPY(dumpi_add_error_string,errorstring)
+		break;
 	case DUMPI_Comm_get_name:
-
+		STRCOPY(dumpi_comm_get_name,name)
+		break;
 	case DUMPI_Comm_set_name:
-
+		STRCOPY(dumpi_comm_set_name,name)
+		break;
 	case DUMPI_Init_thread:
-
+		((dumpi_init_thread*)result)->argv = (char**)malloc(sizeof(char*)*((dumpi_init_thread*)prm)->argc);
+		for(i=0; i<((dumpi_init_thread*)prm)->argc; i++) {
+			STRCOPY(dumpi_init_thread,argv[i])
+		}
+		break;
 	case DUMPI_Type_get_contents:
-
+		COPY(dumpi_type_get_contents,int,arrintegers,numintegers)
+		COPY(dumpi_type_get_contents,int,arraddresses,numdatatypes)
+		break;
 	case DUMPI_Type_get_name:
-
+		STRCOPY(dumpi_type_get_name,name)
+		break;
 	case DUMPI_Type_set_name:
-
+		STRCOPY(dumpi_type_set_name,name)
+		break;
 	case DUMPI_Info_get:
-
+		STRCOPY(dumpi_info_get,key)
+		STRCOPY(dumpi_info_get,value)
+		break;
 	case DUMPI_Info_get_nthkey:
-
+		STRCOPY(dumpi_info_get_nthkey,key)
+		break;
 	case DUMPI_Info_get_valuelen:
-
+		STRCOPY(dumpi_info_get_valuelen,key)
+		break;
 	case DUMPI_Info_set:
-
+		STRCOPY(dumpi_info_set,key)
+		STRCOPY(dumpi_info_set,value)
+		break;
 	case DUMPI_Pack_external:
-
+		STRCOPY(dumpi_pack_external,datarep)
+		break;
 	case DUMPI_Pack_external_size:
-
+		STRCOPY(dumpi_pack_external_size,datarep)
+		break;
 	case DUMPI_Type_create_darray:
-
+		COPY(dumpi_type_create_darray,int,gsizes,ndims)
+		COPY(dumpi_type_create_darray,dumpi_distribution,distribs,ndims)
+		COPY(dumpi_type_create_darray,int,dargs,ndims)
+		COPY(dumpi_type_create_darray,int,psizes,ndims)
+		break;
 	case DUMPI_Type_create_hindexed:
-
+		COPY(dumpi_type_create_hindexed,int,blocklengths,count)
+		COPY(dumpi_type_create_hindexed,int,displacements,count)
+		break;
 	case DUMPI_Type_create_indexed_block:
-
+		COPY(dumpi_type_create_indexed_block,int,displacments,count)
+		break;
 	case DUMPI_Type_create_struct:
-
+		COPY(dumpi_type_create_struct,int,blocklengths,count)
+		COPY(dumpi_type_create_struct,int,displacements,count)
+		COPY(dumpi_type_create_struct,dumpi_datatype,oldtypes,count)
+		break;
 	case DUMPI_Type_create_subarray:
-
+		COPY(dumpi_type_create_subarray,int,sizes,ndims)
+		COPY(dumpi_type_create_subarray,int,subsizes,ndims)
+		COPY(dumpi_type_create_subarray,int,starts,ndims)
+		break;
 	case DUMPI_Unpack_external:
-
+		STRCOPY(dumpi_unpack_external,datarep)
+		break;
 	case DUMPI_File_open:
-
+		STRCOPY(dumpi_file_open,filename)
+		break;
 	case DUMPI_File_delete:
-
+		STRCOPY(dumpi_file_delete,filename)
+		break;
 	case DUMPI_File_set_view:
-
+		STRCOPY(dumpi_file_set_view,datarep)
+		break;
 	case DUMPI_File_get_view:
-
-		fprintf(stderr,"Error in file %s line %d, please see developer",__FILE__,__LINE__);
-		exit(-1);
+		STRCOPY(dumpi_file_get_view,datarep)
+		break;
 	}
 	return result;
 }
