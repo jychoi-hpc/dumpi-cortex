ac8efdf747583f473b15616c1cff326e175b3692
Matthieu Dorier <mdorier@anl.gov>
2016-11-09 13:43:18 -0600

    corrected issue with conversion from stl vector to python list

--- include/cortex/python-util.hpp
+++ include/cortex/python-util.hpp
@@ -5,7 +5,13 @@ namespace py = boost::python;
 
 template<typename T>
 py::list stl_vector_to_py_list(const std::vector<T>& v) {
-	py::object get_iter = py::iterator<std::vector<T> >();
+/*	py::object get_iter = py::iterator<std::vector<T> >();
 	py::object iter = get_iter(v);
 	return py::list(iter);
+*/
+	py::list l;
+	for(int i=0; i<v.size(); i++) {
+		l.append(v[i]);
+	}
+	return l;
 }
